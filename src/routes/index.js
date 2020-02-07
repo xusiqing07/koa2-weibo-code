d6aabaafd83c213ff5e5675a48272eb8c4f0362a
xusiqing <xusiqing@bytedance.com>
2020-02-07 15:35:30 +0800

    feat: 路由的演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,4 +16,21 @@ router.get('/json', async (ctx, next) => {
   }
 })
 
+router.get('/profile/:userName/:name', async (ctx, next) => {
+  const { userName } = ctx.params;
+  ctx.body = {
+    title: 'this is profile',
+    userName,
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { userName,pageIndex } = ctx.params;
+  ctx.body = {
+    title: 'this is loadMore',
+    userName,
+    pageIndex
+  }
+})
+
 module.exports = router
--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,13 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login', async (ctx, next) => {
+  const { userName, passWord } = ctx.request.body
+  ctx.body = {
+    title: 'this is login',
+    userName,
+    passWord
+  }
+})
+
 module.exports = router
