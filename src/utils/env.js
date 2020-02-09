ebd63c524b1684d6e87faec21edceddb651c6bde
xusiqing <xusiqing@bytedance.com>
2020-02-09 12:47:03 +0800

    feat: redis操作

---
+++ src/utils/env.js
@@ -0,0 +1,12 @@
+/**
+ * @description 环境变量
+ */
+
+ const ENV = process.env.NODE_ENV
+
+ module.exports = {
+     isDev: ENV === 'dev',
+     notDev: Env !== 'dev',
+     isProd: Env === 'production',
+     notProd: Env !== 'production'
+ }
