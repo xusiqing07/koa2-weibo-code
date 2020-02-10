b0955700078e7077e89e498c95253dd6bb68d60d
xusiqing <xusiqing@bytedance.com>
2020-02-10 11:49:28 +0800

    feat: 用户数据模型建立

---
+++ src/db/model/index.js
@@ -0,0 +1,9 @@
+/**
+ * @description 数据模型 入口文件
+ */
+
+const User = require('./User')
+
+module.exports = {
+    User
+}
--- src/db/sync.js
+++ src/db/sync.js
@@ -3,6 +3,8 @@
  */
 const seq = require('./seq')
 
+require('./model/index')
+
 //测试连接
 seq.authenticate().then(()=>{
     console.log('ok')
