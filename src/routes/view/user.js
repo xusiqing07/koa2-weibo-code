a01285bd64b70bc2f027085d0aa90a736b443f7f
xusiqing <xusiqing@bytedance.com>
2020-02-10 22:04:59 +0800

    fix: 补充已登录状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -3,10 +3,27 @@
  */
 const router = require('koa-router')()
 
+/**
+ * 获取用户信息
+ * @param {Object} ctx
+ */
+function getUserInfo(ctx) {
+    let data = {
+        isLogin: false
+    }
+    const userInfo = ctx.session.userInfo
+    if(userInfo) {
+        data = {
+            isLogin: true,
+            userName: userInfo.userName
+        }
+    }
+    return data
+}
 router.get('/login', async (ctx, next) => {
-    await ctx.render('login', {})
+    await ctx.render('login', getUserInfo(ctx))
 })
 router.get('/register', async (ctx, next) => {
-    await ctx.render('register', {})
+    await ctx.render('register', getUserInfo(ctx))
 })
 module.exports = router
