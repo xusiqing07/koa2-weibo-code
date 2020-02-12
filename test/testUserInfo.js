bc68d7cd92dbbd421a786231e379f779958e9d6b
xusiqing <xusiqing@bytedance.com>
2020-02-12 13:44:15 +0800

    test: 首页创建微博单元测试

---
+++ test/testUserInfo.js
@@ -0,0 +1,12 @@
+/**
+ * @description 单元测试的用户信息
+ */
+
+/**
+ * 【特别提醒】cookie 是用户的敏感信息，此处只能是**测试**用户的 cookie
+ * 每次测试用户重新登录，都需要更新这里的 cookie
+ */
+
+module.exports = {
+    COOKIE: 'weibo:sid=8PXHoMlMpM1v-HkjwC21eEE8liuDLJo9; weibo:sid.sig=l0hLaqCIMR2CZiJvgdRe54N-3q8'
+}
