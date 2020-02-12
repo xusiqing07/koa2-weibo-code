e3bc13efd3028ee4b993b786e1ff1b94e8083d6e
xusiqing <xusiqing@bytedance.com>
2020-02-12 22:38:15 +0800

    feat: 个人主页加载更多

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const path = require('path')
 const userAPIRouter = require('./routes/api/user')
 const utilsAPIRouter = require('./routes/api/utils')
 const blogHomeAPIRouter = require('./routes/api/blog-home')
+const profileAPIRouter = require('./routes/api/blog-profile')
 
 const errorViewRouter = require('./routes/view/error')
 const userViewRouter = require('./routes/view/user')
@@ -73,6 +74,7 @@ app.use(blogHomeAPIRouter.routes(), blogHomeAPIRouter.allowedMethods())
 
 app.use(userAPIRouter.routes(), userAPIRouter.allowedMethods())
 app.use(utilsAPIRouter.routes(), utilsAPIRouter.allowedMethods())
+app.use(profileAPIRouter.routes(), profileAPIRouter.allowedMethods())
 
 app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 
